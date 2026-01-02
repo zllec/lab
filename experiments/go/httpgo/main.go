61656b1f82f14ce5e4a8b8970480b2da5265ba0d
Leo Claudio <11918571+zllec@users.noreply.github.com>
2026-01-02 08:59:34 +0800

    chore: add boot.dev progress

--- experiments/go/httpgo/main.go
+++ experiments/go/httpgo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,19 +14,42 @@ func main() {
 	}
 
 	var a = make([]byte, 8)
+	var line string
 
 	for {
 		isEOF, err := file.Read(a)
 		if err != nil {
+
 			log.Println(err)
 			return
 		}
 
 		if isEOF == 0 {
 			log.Println("end of file")
+			continue
 		}
 
-		fmt.Printf("read: %s\n", a)
+		if strings.Contains(string(a), "\n") {
+			str := strings.Split(string(a), "\n")
+			lastPart := str[:len(str)-1]
+
+			line = line + strings.Join(str[0:len(str)-1], "")
+			log.Println(line)
+
+			line = ""
+			line = line + strings.Join(lastPart, "")
+			continue
+		}
+
+		line = line + string(a)
+
+		// line = line + string(a)
+		//
+		// if strings.Contains(string(a), "\n") {
+		// 	fmt.Printf("read: %s", line)
+		// 	line = ""
+		// 	continue
+		// }
 	}
 
 }
